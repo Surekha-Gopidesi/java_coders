674f4873a5f8d9f0681ef60ca8abb2699c15a83a
surekha <surekhagopidesi@gmail.com>
2025-12-02 11:48:49 +0530

    Revere of a number

---
+++ javapractice/src/assessment_tesks/ReverseOfNumber.java
@@ -0,0 +1,17 @@
+package assessment_tesks;
+
+public class ReverseOfNumber {
+
+	public static void main(String[] args) {
+		int num=1234;
+		int rev=0;
+		while(num >0) {
+			int rem=num%10;
+			rev=rem+rev*10;
+			num=num/10;
+		}
+		System.out.println("Reverse of a number :"+rev);
+		
+	}
+
+}
